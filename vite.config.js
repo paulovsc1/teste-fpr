119669508b816a14a45a178da77880abd88a269f
Paulo Victor da Silva Carlos <paulopbv2418@gmail.com>
2025-08-29 02:39:56 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/teste-fpr/", // 👈 adiciona isso
+  plugins: [react()],
+  base: "/teste-fpr/", 
 });
